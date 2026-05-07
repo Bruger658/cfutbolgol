30ed8f697c2de0d2367bb452dfd372cefc7b6d47
Bruger658 <sergiospitaleri658@hotmail.com>
2026-05-07 12:29:45 -0300

    Agregar galerías/publicaciones y ajustes de perfil/layout

--- resources/views/noticias/edit.blade.php
+++ resources/views/noticias/edit.blade.php
@@ -4,4 +4,10 @@
         @method('PUT')
         @include('noticias._form', ['publication' => $publication])
     </form>
+
+    <form method="POST" action="{{ route('publications.destroy', $publication) }}" class="max-w-3xl mt-4">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="rounded bg-red-600 px-4 py-2 text-white hover:bg-red-700">Borrar noticia</button>
+    </form>
 </x-layouts.app>
